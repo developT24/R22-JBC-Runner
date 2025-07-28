a9aed640c25c45e9ab80c0f0931d5082671fb80c
Saurav Kumar <sauravkumar045@gmail.com>
2025-07-28 09:24:12 +0800

    Update LandingScreen.java

--- src/main/java/com/t24/gui/LandingScreen.java
+++ src/main/java/com/t24/gui/LandingScreen.java
@@ -4,7 +4,7 @@ package com.t24.gui;
  * TODO: This program is an GUI program to run jBC programs of Temenos. User will be asked to input the jBC routine name to execute it.
  * Can be used to perform corrections with all logic owned in jBC routine to start and end T24 session. 
  *
- * @author bsauravkumar@temenos.com
+ * @author
  *
  */
 public class LandingScreen {
